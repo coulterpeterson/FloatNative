706437e1b679d3ebdb68321ea23ae73f8dc512c4
Lee Sherwood <1732877+leesherwood@users.noreply.github.com>
2026-05-13 09:54:57 +0100

    Update OpenAPI spec and regenerate models to fix subscription decoding error

--- apps/ios/FloatNative/Models/Generated/PlanInfoV2ResponsePlansInner.swift
+++ apps/ios/FloatNative/Models/Generated/PlanInfoV2ResponsePlansInner.swift
@@ -19,7 +19,6 @@ public struct PlanInfoV2ResponsePlansInner: Codable, JSONEncodable, Hashable {
     public var priceYearly: String?
     public var currency: String
     public var logo: String?
-    public var interval: String
     public var featured: Bool
     public var allowGrandfatheredAccess: Bool?
     public var discordServers: [DiscordServerModel]
@@ -34,7 +33,7 @@ public struct PlanInfoV2ResponsePlansInner: Codable, JSONEncodable, Hashable {
     public var userIsGrandfathered: Bool?
     public var enabledGlobal: Bool
 
-    public init(id: String, title: String, description: String, price: String?, priceYearly: String? = nil, currency: String, logo: String?, interval: String, featured: Bool, allowGrandfatheredAccess: Bool? = nil, discordServers: [DiscordServerModel], discordRoles: [DiscordRoleModel], createdAt: Date, updatedAt: Date?, enabled: Bool, paymentID: Int?, trialPeriod: Double, creator: String, userIsSubscribed: Bool, userIsGrandfathered: Bool? = nil, enabledGlobal: Bool) {
+    public init(id: String, title: String, description: String, price: String?, priceYearly: String? = nil, currency: String, logo: String?, featured: Bool, allowGrandfatheredAccess: Bool? = nil, discordServers: [DiscordServerModel], discordRoles: [DiscordRoleModel], createdAt: Date, updatedAt: Date?, enabled: Bool, paymentID: Int?, trialPeriod: Double, creator: String, userIsSubscribed: Bool, userIsGrandfathered: Bool? = nil, enabledGlobal: Bool) {
         self.id = id
         self.title = title
         self.description = description
@@ -42,7 +41,6 @@ public struct PlanInfoV2ResponsePlansInner: Codable, JSONEncodable, Hashable {
         self.priceYearly = priceYearly
         self.currency = currency
         self.logo = logo
-        self.interval = interval
         self.featured = featured
         self.allowGrandfatheredAccess = allowGrandfatheredAccess
         self.discordServers = discordServers
@@ -66,7 +64,6 @@ public struct PlanInfoV2ResponsePlansInner: Codable, JSONEncodable, Hashable {
         case priceYearly
         case currency
         case logo
-        case interval
         case featured
         case allowGrandfatheredAccess
         case discordServers
@@ -93,7 +90,6 @@ public struct PlanInfoV2ResponsePlansInner: Codable, JSONEncodable, Hashable {
         try container.encodeIfPresent(priceYearly, forKey: .priceYearly)
         try container.encode(currency, forKey: .currency)
         try container.encode(logo, forKey: .logo)
-        try container.encode(interval, forKey: .interval)
         try container.encode(featured, forKey: .featured)
         try container.encodeIfPresent(allowGrandfatheredAccess, forKey: .allowGrandfatheredAccess)
         try container.encode(discordServers, forKey: .discordServers)
--- apps/ios/FloatNative/Models/Generated/SubscriptionPlanModel.swift
+++ apps/ios/FloatNative/Models/Generated/SubscriptionPlanModel.swift
@@ -19,13 +19,12 @@ public struct SubscriptionPlanModel: Codable, JSONEncodable, Hashable {
     public var priceYearly: String?
     public var currency: String
     public var logo: String?
-    public var interval: String
     public var featured: Bool
     public var allowGrandfatheredAccess: Bool?
     public var discordServers: [DiscordServerModel]
     public var discordRoles: [DiscordRoleModel]
 
-    public init(id: String, title: String, description: String, price: String?, priceYearly: String? = nil, currency: String, logo: String?, interval: String, featured: Bool, allowGrandfatheredAccess: Bool? = nil, discordServers: [DiscordServerModel], discordRoles: [DiscordRoleModel]) {
+    public init(id: String, title: String, description: String, price: String?, priceYearly: String? = nil, currency: String, logo: String?, featured: Bool, allowGrandfatheredAccess: Bool? = nil, discordServers: [DiscordServerModel], discordRoles: [DiscordRoleModel]) {
         self.id = id
         self.title = title
         self.description = description
@@ -33,7 +32,6 @@ public struct SubscriptionPlanModel: Codable, JSONEncodable, Hashable {
         self.priceYearly = priceYearly
         self.currency = currency
         self.logo = logo
-        self.interval = interval
         self.featured = featured
         self.allowGrandfatheredAccess = allowGrandfatheredAccess
         self.discordServers = discordServers
@@ -48,7 +46,6 @@ public struct SubscriptionPlanModel: Codable, JSONEncodable, Hashable {
         case priceYearly
         case currency
         case logo
-        case interval
         case featured
         case allowGrandfatheredAccess
         case discordServers
@@ -66,7 +63,6 @@ public struct SubscriptionPlanModel: Codable, JSONEncodable, Hashable {
         try container.encodeIfPresent(priceYearly, forKey: .priceYearly)
         try container.encode(currency, forKey: .currency)
         try container.encode(logo, forKey: .logo)
-        try container.encode(interval, forKey: .interval)
         try container.encode(featured, forKey: .featured)
         try container.encodeIfPresent(allowGrandfatheredAccess, forKey: .allowGrandfatheredAccess)
         try container.encode(discordServers, forKey: .discordServers)
